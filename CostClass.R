bbfc6dbcb1c8e86a9919b9cbf09d49c25747afe6
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-06-09 22:02:16 +0100

    Create CostClass.R

---
+++ CostClass.R
@@ -0,0 +1,45 @@
+library(R6)
+
+Cost <- R6Class("Cost", list(
+  value = 10,
+  year = 2000,
+  currency = "GBP",
+  base_currency = "GBP",
+
+  convert_currency = function(to_currency) {
+    self$value <- self$value + 1
+    self$currency <- to_currency
+    invisible(self)
+  },
+  currency_to_base_country = function() {
+    self$change_currency(self$base_currency)  
+    invisible(self)
+  },
+  change_year = function(year) {
+    temp_currency <- self$currency
+    self$currency_to_base_country()
+    self$inflate_value(year)
+    self$change_currency(temp_currency)
+    invisible(self)
+  },
+  inflate_value = function(year) {
+    self$value <- self$value*(1 + 0.035)^(year - self$year)
+    self$year <- year
+    invisible(self)
+  }
+))
+
+mycost <- Cost$new()
+
+mycost$value
+mycost$change_currency("USD")
+mycost$currency
+
+mycost$currency_to_base_country()
+mycost$currency
+
+mycost$inflate_value(2020)
+mycost$value
+
+mycost$change_year(2020)
+mycost$value
